0e0f4c8227371b4952cfb277a3cd1a3b64449a48
niravpatell23 <nnirav.patel7@gmail.com>
2024-10-14 20:55:32 -0400

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+import nextConfig from "./next.config.js";
+import webpack from "./webpack.config.mjs";
+
+nextConfig.webpack = webpack;
 
 export default nextConfig;
